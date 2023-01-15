40e183b64477fa546dfb41f3a0e623b417e48a0f
Joshua Jeschek <joshua@jeschek.eu>
2023-01-15 12:38:14 +0100

    fix: new install

--- update-custom-css.sh
+++ update-custom-css.sh
@@ -1,1 +1,1 @@
-cp /home/josh/.mozilla/firefox/mtmovkqw.dev-edition-default/chrome/customChrome.css /home/josh/opt/WhiteSur-gtk-theme/src/other/firefox/customChrome.css
+cp /home/josh/.mozilla/firefox/27rm7klx.dev-edition-default/chrome/customChrome.css /home/josh/opt/WhiteSur-gtk-theme/src/other/firefox/customChrome.css
